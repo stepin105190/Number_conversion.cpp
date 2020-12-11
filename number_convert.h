e9af95134254eaf7ebfffba23f33d88b552b67e1
stepin105190 <69082292+stepin105190@users.noreply.github.com>
2020-12-11 21:04:16 +0530

    Add files via upload

---
+++ number_convert.h
@@ -0,0 +1,9 @@
+#ifndef  __NUMBER_CONVERT_H__
+	#define __NUMBER_CONVERT_H__
+	int Binary_to_Decimal(long long n);
+	long Decimal_to_Binary(int n);
+	int Decimal_to_Octal(int decimal);
+	long Binary_to_Octal(long long binaryNumber);
+	int Octal_to_Decimal(int octal);
+
+	#endif
